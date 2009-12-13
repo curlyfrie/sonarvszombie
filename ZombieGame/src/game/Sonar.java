b5a8e85cc8337693558ea51d4f27f761bdd0bc69
e0525640@student.tuwien.ac.at <e0525640@student.tuwien.ac.at@f4c9227c-e703-11de-b136-e12340cf38ae>
2009-12-13 13:38:02 +0000

    Fix NullpointerException @ removeAllEnemies().

--- ZombieGame/src/game/Sonar.java
+++ ZombieGame/src/game/Sonar.java
@@ -163,11 +163,9 @@ public class Sonar extends PApplet {
 		if (!enemies.isEmpty()) {
 			Collection<ArrayList<Enemy>> temp = enemies.values();
 
-			for (int i = 0; i < temp.size(); i++) {
-				for (ArrayList<Enemy> listToDraw : temp) {
-					for (int j = 0; j < listToDraw.size(); j++) {
-						listToDraw.get(j).stopSound();
-					}
+			for (ArrayList<Enemy> listToDraw : temp) {
+				for (int j = 0; j < listToDraw.size(); j++) {
+					listToDraw.get(j).stopSound();
 				}
 			}
 		}
